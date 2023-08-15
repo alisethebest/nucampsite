e0877913ed0a70485a7fdc5c423e31c32468a059
Alise Cervantes <alise09@icloud.com>
2023-08-14 18:08:36 -0700

    nucamp site

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+console.log("javascript connected!");
+
+const carouselElement = document.getElementById("homeCarousel");
+const carousel = new bootstrap.Carousel(carouselElement, {
+  interval: 2000,
+  pause: false,
+});
+
+// when the pause button is clicked, pause the carousel
+const carouselPause = document.getElementById("carouselPause");
+carouselPause.addEventListener("click", function () {
+  console.log("pausing the carousel");
+  carousel.pause();
+});
+
+// when the play button is clicked, begin cycling through the images
+const carouselPlay = document.getElementById("carouselPlay");
+carouselPlay.addEventListener("click", function () {
+  console.log("cycle the carousel");
+  carousel.cycle();
+});
